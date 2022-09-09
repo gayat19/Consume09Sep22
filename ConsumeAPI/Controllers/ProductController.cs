2b80c278c535aa86997a722717a861a7d3439be2
Gayathri Mahadevan <gayat_19@yahoo.com>
2022-09-09 19:03:01 +0530

    Consume

---
+++ ConsumeAPI/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using ConsumeAPI.Models;
+using ConsumeAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumeAPI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _service;
+
+        public ProductController(ProductService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _service.GetAll();
+            return View(result);
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            var result = await _service.Add(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ConsumeAPI/Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using ConsumeAPI.Models;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace ConsumeAPI.Services
+{
+    public class ProductService
+    {
+        HttpClient client;
+        public ProductService()
+        {
+            //client = new HttpClient();
+        }
+        public async Task<ICollection<Product>> GetAll()
+        {
+            List<Product> products = null;
+            try
+            {
+                using (client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:31457/");
+                    var response = await client.GetAsync("api/Product");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseData = response.Content.ReadAsStringAsync().Result;
+                        products = JsonConvert.DeserializeObject<List<Product>>(responseData);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+            return products;
+        }
+        public async Task<Product> Add(Product prod)
+        {
+            Product product = null;
+            try
+            {
+                using (client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:31457/");
+                    var response = await client.PostAsJsonAsync("api/Product",prod);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseData = response.Content.ReadAsStringAsync().Result;
+                        product = JsonConvert.DeserializeObject<Product>(responseData);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+            return product;
+        }
+     }
+}
